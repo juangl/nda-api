fb14826139731d89556e8623206396baf3b472e1
osvaguillen <oswmargui50@hotmail.com>
2020-02-03 20:12:45 -0600

    Removed try/catches from likedItems routes

--- routes/likedItems/get/likedItems/index.js
+++ routes/likedItems/get/likedItems/index.js
@@ -3,12 +3,8 @@ const { authorize, grantAccess } = require('../../../../middlewares');
 const { db } = require('../../../../utils');
 
 const handler = async (req, res) => {
-  try {
-    const userId = req.locals.user.id;
-    res.respond(await db.namespaces.likedItems.getLiked(userId));
-  } catch (e) {
-    res.respond(e);
-  }
+  const userId = req.locals.user.id;
+  res.respond(await db.namespaces.likedItems.getLiked(userId));
 };
 
 module.exports = compose([authorize, grantAccess, handler]);
--- routes/likedItems/post/likedItem/index.js
+++ routes/likedItems/post/likedItem/index.js
@@ -9,27 +9,23 @@ const {
 const sanitizer = createSanitizer(shapes.likedItem, true);
 
 const handler = async (req, res) => {
-  try {
-    const userId = req.locals.user.id;
-    const entityType = req.body.type;
-    debug(
-      `User with id ${userId} is adding a new ${entityType} to its liked items`,
-    );
-    req.body.userId = userId;
-    res.respond(await db.utils.insert('likedItems', req.body), error => {
-      if (error) {
-        debug(
-          `User with id ${userId} has failed by adding a new item to its liked items`,
-        );
-      } else {
-        debug(
-          `User with id ${userId} has liked a ${entityType} with id ${req.body.id}`,
-        );
-      }
-    });
-  } catch (e) {
-    res.respond(e);
-  }
+  const userId = req.locals.user.id;
+  const entityType = req.body.type;
+  debug(
+    `User with id ${userId} is adding a new ${entityType} to its liked items`,
+  );
+  req.body.userId = userId;
+  res.respond(await db.utils.insert('likedItems', req.body), error => {
+    if (error) {
+      debug(
+        `User with id ${userId} has failed by adding a new item to its liked items`,
+      );
+    } else {
+      debug(
+        `User with id ${userId} has liked a ${entityType} with id ${req.body.id}`,
+      );
+    }
+  });
 };
 
 module.exports = compose([authorize, grantAccess, sanitizer, handler]);
